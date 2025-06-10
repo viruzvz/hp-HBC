4637e26ca6d3d8ba7a52cf895ead322975f4f601
@cegn <cegn@senado.leg.br>
2025-06-09 21:27:05 -0300

    gh-pages

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-import 'lazysizes';
-// import a plugin
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-// import lazyframe from 'lazyframe'
-import '/src/js/cookie-consent.js'
